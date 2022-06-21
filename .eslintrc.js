70b828e31aa1df786e6c605181dd57750d3093e2
John Doe <johndoe@example.com>
2022-06-21 13:14:00 +0300

    pre-config

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+/* eslint-env node */
+module.exports = {
+  "env": {
+    "node": true,
+    "es6": true
+  },
+  "extends": [ "eslint:recommended" ],
+  "parserOptions": {
+    "ecmaVersion": 9
+  }
+}
---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+/* eslint-env node */
+module.exports = {
+  "extends": [ "stylelint-config-standard-scss" ],
+  "rules": {
+    "selector-class-pattern": null,
+    "property-no-vendor-prefix": null
+  }
+}
